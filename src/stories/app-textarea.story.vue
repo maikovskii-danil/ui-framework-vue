fe8b8211e60d58b11f4907cc66438ba3dd2a456f
Daniil Maykovskiy <daniil.maikovsky@yandex.ru>
2025-07-16 16:45:42 +0300

    add textarea

---
+++ src/stories/app-textarea.story.vue
@@ -0,0 +1,47 @@
+<template>
+  <Story
+    id="AppTextarea"
+    responsive-disabled
+    title="AppTextarea"
+    :layout="{ type: 'grid', width: '40%' }"
+  >
+    <Variant title="default">
+      <div class="flex flex-col gap-4">
+        <div>AppInput:</div>
+        <AppInput
+          v-model="inputValue"
+          placeholder="Enter text"
+        />
+        <div>AppTextarea:</div>
+        <AppTextarea
+          v-model="textareaValue"
+          placeholder="Enter text"
+        />
+      </div>
+    </Variant>
+    <Variant title="disabled">
+      <div class="flex flex-col gap-4">
+        <div>AppInput:</div>
+        <AppInput
+          disabled
+          placeholder="Enter text"
+        />
+        <div>AppTextarea:</div>
+        <AppTextarea
+          disabled
+          placeholder="Enter text"
+        />
+      </div>
+    </Variant>
+  </Story>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+import AppInput from '@/uikit/app-input.vue';
+import AppTextarea from '@/uikit/app-textarea.vue';
+
+const inputValue = ref('');
+const textareaValue = ref('');
+</script>
